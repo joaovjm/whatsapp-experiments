88d3460a4d72da2561d61ecd0d90fda038affc35
joaovjm <joao.oliveira18.jm@gmail.com>
2025-09-18 23:35:37 -0300

    log test

--- api/whatsapp.js
+++ api/whatsapp.js
@@ -1,13 +1,16 @@
 // pages/api/whatsapp.js
+import dotenv from "dotenv";
+dotenv.config();
 
 export default function handler(req, res) {
     if (req.method === "GET") {
       // Verificação do webhook
       const verify_token = process.env.WEBHOOK_VERIFY_TOKEN;
+      console.log("Tokend e verificação do webhook: ", process.env.WEBHOOK_VERIFY_TOKEN)
       const mode = req.query["hub.mode"];
       const token = req.query["hub.verify_token"];
       const challenge = req.query["hub.challenge"];
-  
+        
       if (mode && token && mode === "subscribe" && token === verify_token) {
         console.log("WEBHOOK VERIFICADO");
         res.status(200).send(challenge);
