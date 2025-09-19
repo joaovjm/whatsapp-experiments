94a7344debfddeedfeb2d02161b8dc93c1c8d2e1
joaovjm <joao.oliveira18.jm@gmail.com>
2025-09-19 00:21:33 -0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,8 @@ import { useEffect, useState } from 'react'
 function App() {
   const [message, setMessage] = useState([])
 
-  useEffect(() => {
-    fetch("/api/whatsapp")
-    .then(response => response.json())
-    .then(data => setMessage(data))
-  }, [])
-
   return (
     <>
-      {message}
     </>
   )
 }
