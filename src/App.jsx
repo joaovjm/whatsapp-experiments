b27bb221330de8efb5cd6236d0acb12069a82d5c
joaovjm <joao.oliveira18.jm@gmail.com>
2025-09-18 23:45:51 -0300

    Front

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    fetch("/api/whatsapp")
+    .then(response => response.json())
+    .then(data => setMessage(data))
+  }, [])
 
   return (
     <>
+      {message?.map((msg) => (
+        <div key={msg.id}>{msg.message}</div>
+      ))}
     </>
   )
 }
