dfe22be5b4527e9d30811a19bcb409d266438f7f
joaovjm <joao.oliveira18.jm@gmail.com>
2025-09-19 01:13:17 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,22 @@ import { useEffect, useState } from 'react'
 function App() {
   const [message, setMessage] = useState([])
 
+  useEffect(() => {
+    const eventSource = new EventSource("/api/whatsapp-stream");
+    eventSource.onmessage = (event) => {
+      const newMessage = JSON.parse(event.data);
+      setMessage((prev) => [...prev, newMessage]);
+    };
+    return () => {
+      eventSource.close();
+    };
+  }, []);
   return (
     <>
+    <h2>Mensagem Whatsapp</h2>
+      {message.map((msg, index) => (
+        <div key={index}>{JSON.stringify(msg)}</div>
+      ))}
     </>
   )
 }
