49236dfa5ecb078afb793b8990a245337f414bbb
joaovjm <joao.oliveira18.jm@gmail.com>
2025-09-23 01:08:30 -0300

    commit 44

--- api/send-message.js
+++ api/send-message.js
@@ -18,9 +18,9 @@ export default async function handler(req, res) {
       })
     });
   
-    const data = await response.json();
+    const dt = await response.json();
  
-    if (data){
+    if (dt.ok){
       const { data, error } = await supabase.from('messages').insert([{
         from: process.env.WHATSAPP_PHONE_NUMBER,
         to: to,
@@ -33,6 +33,6 @@ export default async function handler(req, res) {
       }
     }
     //res.status(response.ok ? 200 : 500).json(data);
-    res.status(200).json(data);
+    res.status(200).json(dt);
   }
   
