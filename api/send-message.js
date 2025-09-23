d31f5f145518ca35f26edd843bbe3e3cf78637f0
joaovjm <joao.oliveira18.jm@gmail.com>
2025-09-23 02:18:40 -0300

    commit 54

--- api/send-message.js
+++ api/send-message.js
@@ -1,3 +1,5 @@
+import supabase from "../src/helper/superBaseClient";
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método não permitido" });
