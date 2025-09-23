e81fe243017b027790aa04f63b7e273292eed66d
joaovjm <joao.oliveira18.jm@gmail.com>
2025-09-22 22:31:00 -0300

    commit 28

--- src/services/formatPhone.js
+++ src/services/formatPhone.js
@@ -1,5 +1,9 @@
 function formatPhone(phone) {
-  return phone.replace(/\D/g, '').replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+    let phoneFormatted = phone;
+    if(phoneFormatted.startsWith('55')){
+        phoneFormatted = phoneFormatted.slice(2);
+    }
+  return phoneFormatted.replace(/\D/g, '').replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
 }
 
 export default formatPhone;
