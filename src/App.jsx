850ca6511020146234530f743b2d821bf302e297
joaovjm <joao.oliveira18.jm@gmail.com>
2025-09-23 00:43:37 -0300

    commit 41

--- src/App.jsx
+++ src/App.jsx
@@ -29,14 +29,13 @@ export default function Chat() {
     const to = '5521983046033';
     const type = 'text';
     const message = input;
-    const msg = { to, type, message };
-    console.log(msg);
+
     const response = await fetch('/api/send-message', {
       method: 'POST',
       body: JSON.stringify({to, type, message}),
       headers: { 'Content-Type': 'application/json' }
     });
-    console.log(await response.json());
+    
     setInput('');
   };
 
